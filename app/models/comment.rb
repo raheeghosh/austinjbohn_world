a1ef86515a090dc0a594334233355f0cbece784a
Rahee Ghosh <rghosh@andrew.cmu.edu>
2012-04-16 20:18:56 -0400

    Added comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ActiveRecord::Base
+  
+  validates_presence_of :content, :name, :email
+  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/
+  
 end
